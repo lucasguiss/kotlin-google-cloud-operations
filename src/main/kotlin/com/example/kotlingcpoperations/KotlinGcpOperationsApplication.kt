364eb36141205a28dabcf554807d5e5467a7a254
Lucas Guiss Gusmão <lucasguissgusmao@gmail.com>
2021-07-18 14:09:04 -0300

    chore(app): initializing project

---
+++ src/main/kotlin/com/example/kotlingcpoperations/KotlinGcpOperationsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlingcpoperations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinGcpOperationsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinGcpOperationsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlingcpoperations/KotlinGcpOperationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlingcpoperations
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinGcpOperationsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
